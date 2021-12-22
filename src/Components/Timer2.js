65b23bcff71cb5faa6a747fdcd5d1c279f49d246
John <johngemmell124@gmail.com>
2021-12-21 22:55:29 -0500

    added high score

--- src/Components/Timer2.js
+++ src/Components/Timer2.js
@@ -7,11 +7,20 @@ export default function Timer2({timer, numChars, isFinished}) {
   const [didStart, setDidStart] = useState(0);  // keep track if game is in progress
   const [startDate, setStartDate] = useState(0);  // keep track of the number seconds since starting
   const [seconds, setSeconds] = useState(0);
-
+  const [score, setScore ] = useState(0);
+  
   
   useEffect(() => {
     setTimeout(() => getTime(), 500);
   });
+
+  // useEffect(() => {
+  //   const words = Math.ceil(numChars / 5.1);
+  //   let currentScore = Math.ceil((words / (seconds)) * 60000);
+  //   if (currentScore > score) {
+  //     setScore(currentScore);
+  //   }
+  // }, [numChars]);
 
 
   function getTime() {
@@ -36,7 +45,17 @@ export default function Timer2({timer, numChars, isFinished}) {
       setSeconds(totalSeconds);
       //console.log("total seconds: " + totalSeconds);
     }
+    else {
+      const words = Math.ceil(numChars / 5.1);
+      let currentScore = Math.ceil((words / (seconds)) * 60000);
+      if (currentScore > score) {
+        setScore(currentScore);
+      }
+    }
   }
+
+
+
 
 
   const displayWPM = () => {
@@ -52,14 +71,29 @@ export default function Timer2({timer, numChars, isFinished}) {
         <h1> 
           {"WPM: 00"}
         </h1>
-        );}
+        );}   
     }
 
+    const displayStartMessage = () => {
+      if (!didStart) {
+        return (<h1 style={{textAlign: "center"}}> Type to Start</h1>)
+      }
+      else if (isFinished) {
+        return (<h1 style={{textAlign: "center"}}> Press 'Restart' to Play Again</h1>)
+      }
+      else if (timer) {
+        return (<h1 style={{textAlign: "center"}}> Type to the finish line!</h1>)
+      }
+    }
   
   return (
     <div className="timer">
       <h1>Total Time: {seconds / 1000}</h1>
       {displayWPM()}
+      <h1>
+        Best Speed: {score}  <button onClick={(() => setScore(0))}>Reset Score</button>
+      </h1>
+      {displayStartMessage()}
     </div>
   );
 }
